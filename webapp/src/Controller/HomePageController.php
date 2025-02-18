33f831b592f16ed142f5fd7906fd5fde8105962f
Robin Kiethe <robinkiethe@web.de>
2025-02-18 14:58:18 +0100

    added search bar

--- webapp/src/Controller/HomePageController.php
+++ webapp/src/Controller/HomePageController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Form\SearchFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(SearchFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+        }
+
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'form' => $form->createView(),
         ]);
     }
 }
