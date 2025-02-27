6eb67dbb9720c5b3f169e2892b416ef49edcca12
Robin Kiethe <robinkiethe@web.de>
2025-02-27 12:54:55 +0100

    repaierd migration

--- webapp/migrations/Version20250227115428.php
+++ webapp/migrations/Version20250227115428.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250227115218 extends AbstractMigration
+final class Version20250227115428 extends AbstractMigration
 {
     public function getDescription(): string
     {
